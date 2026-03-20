b82246902fb39c037a3c58dcca0f9bf008c53d29
Pamarthi Nithin <nithinp342@gmail.com>
2026-03-20 20:22:39 +0530

    Add files via upload

---
+++ detecting side effects/Service Provider/views.py
@@ -0,0 +1,218 @@
+
+from django.db.models import  Count, Avg
+from django.shortcuts import render, redirect
+from django.db.models import Count
+from django.db.models import Q
+import datetime
+import xlwt
+from django.http import HttpResponse
+
+import pandas as pd
+
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import f1_score
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import VotingClassifier
+# Create your views here.
+from Remote_User.models import ClientRegister_Model,detect_drug_reactions,detection_ratio,detection_accuracy
+
+
+def serviceproviderlogin(request):
+    if request.method  == "POST":
+        admin = request.POST.get('username')
+        password = request.POST.get('password')
+        if admin == "Admin" and password =="Admin":
+            return redirect('View_Remote_Users')
+
+    return render(request,'SProvider/serviceproviderlogin.html')
+
+def View_Detected_Drug_Reactions_Type_Ratio(request):
+    detection_ratio.objects.all().delete()
+    ratio = ""
+    kword = 'Found More Side Effects'
+    print(kword)
+    obj = detect_drug_reactions.objects.all().filter(Q(Prediction=kword))
+    obj1 = detect_drug_reactions.objects.all()
+    count = obj.count();
+    count1 = obj1.count();
+    ratio = (count / count1) * 100
+    if ratio != 0:
+        detection_ratio.objects.create(names=kword, ratio=ratio)
+
+    ratio1 = ""
+    kword1 = 'No Side Effects'
+    print(kword1)
+    obj1 = detect_drug_reactions.objects.all().filter(Q(Prediction=kword1))
+    obj11 = detect_drug_reactions.objects.all()
+    count1 = obj1.count();
+    count11 = obj11.count();
+    ratio1 = (count1 / count11) * 100
+    if ratio1 != 0:
+        detection_ratio.objects.create(names=kword1, ratio=ratio1)
+
+    obj = detection_ratio.objects.all()
+    return render(request, 'SProvider/View_Detected_Drug_Reactions_Type_Ratio.html', {'objs': obj})
+
+def View_Remote_Users(request):
+    obj=ClientRegister_Model.objects.all()
+    return render(request,'SProvider/View_Remote_Users.html',{'objects':obj})
+
+def ViewTrendings(request):
+    topic = detect_drug_reactions.objects.values('topics').annotate(dcount=Count('topics')).order_by('-dcount')
+    return  render(request,'SProvider/ViewTrendings.html',{'objects':topic})
+
+def charts(request,chart_type):
+    chart1 = detection_ratio.objects.values('names').annotate(dcount=Avg('ratio'))
+    return render(request,"SProvider/charts.html", {'form':chart1, 'chart_type':chart_type})
+
+def charts1(request,chart_type):
+    chart1 = detection_accuracy.objects.values('names').annotate(dcount=Avg('ratio'))
+    return render(request,"SProvider/charts1.html", {'form':chart1, 'chart_type':chart_type})
+
+def View_Detected_Drug_Reactions_Type(request):
+    obj =detect_drug_reactions.objects.all()
+    return render(request, 'SProvider/View_Detected_Drug_Reactions_Type.html', {'list_objects': obj})
+
+def likeschart(request,like_chart):
+    charts =detection_accuracy.objects.values('names').annotate(dcount=Avg('ratio'))
+    return render(request,"SProvider/likeschart.html", {'form':charts, 'like_chart':like_chart})
+
+
+def Download_Predicted_DataSets(request):
+
+    response = HttpResponse(content_type='application/ms-excel')
+    # decide file name
+    response['Content-Disposition'] = 'attachment; filename="Predicted_Data.xls"'
+    # creating workbook
+    wb = xlwt.Workbook(encoding='utf-8')
+    # adding sheet
+    ws = wb.add_sheet("sheet1")
+    # Sheet header, first row
+    row_num = 0
+    font_style = xlwt.XFStyle()
+    # headers are bold
+    font_style.font.bold = True
+    # writer = csv.writer(response)
+    obj = detect_drug_reactions.objects.all()
+    data = obj  # dummy method to fetch data.
+    for my_row in data:
+        row_num = row_num + 1
+
+        ws.write(row_num, 0, my_row.Fid, font_style)
+        ws.write(row_num, 1, my_row.Drug_Name1, font_style)
+        ws.write(row_num, 2, my_row.Reason1, font_style)
+        ws.write(row_num, 3, my_row.Drug_Name2, font_style)
+        ws.write(row_num, 4, my_row.Reason2, font_style)
+        ws.write(row_num, 5, my_row.Rating, font_style)
+        ws.write(row_num, 6, my_row.Reviews, font_style)
+        ws.write(row_num, 7, my_row.Drugfollowers, font_style)
+        ws.write(row_num, 8, my_row.Prediction, font_style)
+
+    wb.save(response)
+    return response
+
+def Train_Test_DataSets(request):
+    detection_accuracy.objects.all().delete()
+
+    data = pd.read_csv("Datasets.csv")
+
+    def apply_results(label):
+        if (label == 0):
+            return 0  # No Side Effects
+        else:
+            return 1  # Found More Side Effects
+
+    data['Label'] = data['Label'].apply(apply_results)
+
+    x = data['Fid']
+    y = data['Label']
+
+    cv = CountVectorizer()
+
+    x = cv.fit_transform(x)
+    models = []
+    from sklearn.model_selection import train_test_split
+    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.20)
+    X_train.shape, X_test.shape, y_train.shape
+
+    print("Graph Neural Network (GNN)")
+
+    from sklearn.neural_network import MLPClassifier
+    mlpc = MLPClassifier().fit(X_train, y_train)
+    y_pred = mlpc.predict(X_test)
+    print("ACCURACY")
+    print(accuracy_score(y_test, y_pred) * 100)
+    print("CLASSIFICATION REPORT")
+    print(classification_report(y_test, y_pred))
+    print("CONFUSION MATRIX")
+    print(confusion_matrix(y_test, y_pred))
+    models.append(('MLPClassifier', mlpc))
+    detection_accuracy.objects.create(names="Graph Neural Network (GNN)",
+                                      ratio=accuracy_score(y_test, y_pred) * 100)
+    # SVM Model
+    print("SVM")
+    from sklearn import svm
+    lin_clf = svm.LinearSVC()
+    lin_clf.fit(X_train, y_train)
+    predict_svm = lin_clf.predict(X_test)
+    svm_acc = accuracy_score(y_test, predict_svm) * 100
+    print(svm_acc)
+    print("CLASSIFICATION REPORT")
+    print(classification_report(y_test, predict_svm))
+    print("CONFUSION MATRIX")
+    print(confusion_matrix(y_test, predict_svm))
+    models.append(('svm', lin_clf))
+    detection_accuracy.objects.create(names="SVM", ratio=svm_acc)
+
+
+    print("Logistic Regression")
+    from sklearn.linear_model import LogisticRegression
+    reg = LogisticRegression(random_state=0, solver='lbfgs').fit(X_train, y_train)
+    y_pred = reg.predict(X_test)
+    print("ACCURACY")
+    print(accuracy_score(y_test, y_pred) * 100)
+    print("CLASSIFICATION REPORT")
+    print(classification_report(y_test, y_pred))
+    print("CONFUSION MATRIX")
+    print(confusion_matrix(y_test, y_pred))
+    models.append(('logistic', reg))
+    detection_accuracy.objects.create(names="Logistic Regression", ratio=accuracy_score(y_test, y_pred) * 100)
+
+
+    print("Decision Tree Classifier")
+    dtc = DecisionTreeClassifier()
+    dtc.fit(X_train, y_train)
+    dtcpredict = dtc.predict(X_test)
+    print("ACCURACY")
+    print(accuracy_score(y_test, dtcpredict) * 100)
+    print("CLASSIFICATION REPORT")
+    print(classification_report(y_test, dtcpredict))
+    print("CONFUSION MATRIX")
+    print(confusion_matrix(y_test, dtcpredict))
+    models.append(('DecisionTreeClassifier', dtc))
+    detection_accuracy.objects.create(names="Decision Tree Classifier", ratio=accuracy_score(y_test, dtcpredict) * 100)
+
+
+    print("KNeighborsClassifier")
+    from sklearn.neighbors import KNeighborsClassifier
+    kn = KNeighborsClassifier()
+    kn.fit(X_train, y_train)
+    knpredict = kn.predict(X_test)
+    print("ACCURACY")
+    print(accuracy_score(y_test, knpredict) * 100)
+    print("CLASSIFICATION REPORT")
+    print(classification_report(y_test, knpredict))
+    print("CONFUSION MATRIX")
+    print(confusion_matrix(y_test, knpredict))
+    models.append(('KNeighborsClassifier', kn))
+    detection_accuracy.objects.create(names="KNeighborsClassifier", ratio=accuracy_score(y_test, knpredict) * 100)
+
+
+    predicts = 'Labels.csv'
+    data.to_csv(predicts, index=False)
+    data.to_markdown
+
+    obj = detection_accuracy.objects.all()
+    return render(request,'SProvider/Train_Test_DataSets.html', {'objs': obj})
